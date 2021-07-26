5e44d05f7cd19957d272e77981f9f5f9d6f77c2e
Adam Gardner <AdamGardner33@gmail.com>
2021-07-26 15:51:12 -0400

    updated app.js to see difference post cd pipeline update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Big Bad World!'));
+app.get('/', (req, res) => res.send('Hello Big Bad World!...now with ci/cd pipeline 🧐'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
